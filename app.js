7da9f21d93f9105f4f2af2c3f988d8606d93439c
Michael Erskine <michael.erskine@ketech.com>
2016-08-26 15:11:21 +0100

    Passing data as status items and 2D table
    Signed-off-by: Michael Erskine <michael.erskine@ketech.com>

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.locals.cmu.loadCmu(major_mapping);
 app.locals.pidig = require('./pidig');
 app.locals.pidig.loadTenThousand();
 
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
 // map string of phonemes to major digits
 function major_mapping(phones) {
     var m = app.locals.maj.majmap;
@@ -50,25 +53,30 @@ app.get('/procdigcmu/', function(req, res) {
     console.log("scan of '"+digits+"' index "+i);
     
     var minimumDigits = 2;
+    var cmucount = app.locals.cmu.getCmuCount();
+
     var result_list = [];
+    var full_data = { 
+        'scanstatus':'finished', 
+        'newindex':0, 
+        'cmucount':cmucount, 
+        'results_tab': result_list 
+    };
     
-    var c = app.locals.cmu.getCmuCount();
-    res.type('text/plain');
-    // TODO validate localcounter as number, etc.
-    if(i < 0 || i >= c) {
-        result_list.push({firstName:'finished!', lastName: 'at '+c, age:13});        
-        res.json(result_list);
-        return;
-    }
     var maxtime = 4000;
     // TODO grab time
     var t1 = process.hrtime();
-    var maxpage = 300000;
+    var maxpage = 100000;
+    // TODO validate localcounter as number, etc.
+    if(i < 0 || i >= cmucount) {
+        full_data.scanstatus = "out of range "+i;
+        i = cmucount;
+    }
     var counter = 0;
-    for(;i<c;i++){
+    for(; i < cmucount; i++){
         counter++;
         if(counter > maxpage) {
-            result_list.push({firstName:'maxpage break!', lastName: 'index is now ' + i, age:13});
+            full_data.scanstatus = "maxpage break " + maxpage;
             break;
         }
         var e = app.locals.cmu.getItem(i);
@@ -82,14 +90,18 @@ app.get('/procdigcmu/', function(req, res) {
         // decorate digits string with <before>[<found word>]<after>
         var decorated_digits = digits.split(e[2]).join("["+e[2]+"]");
         e.push(pos, i, decorated_digits);
-        result_list.push({firstName:''+e, lastName: 'of ' + maxpage, age:13});
+        result_list.push(e);
+        // result_list.push({firstName:''+e, lastName: 'of ' + maxpage, age:13});
     }
+    full_data.newindex = i;
     var dur = process.hrtime(t1);
     console.info("Execution time (hr): %ds %dms", dur[0], dur[1]/1000000);
 
     //~ var s = JSON.stringify(result_list);
     //~ console.log(s);
-    res.json(result_list);
+    
+    res.type('text/plain');
+    res.json(full_data);
 });
 
 // catch 404 and forward to error handler
--- public/javascripts/procdigcmu.js
+++ public/javascripts/procdigcmu.js
@@ -1,4 +1,4 @@
-    var localcounter = 1;
+    var localcounter = 0;
     function getAndRenderData()  {
         $.ajax({
             url: "/procdigcmu?digits=" + digits + "&localcounter=" + localcounter,
@@ -9,12 +9,20 @@
                 // the type of data we expect back
             success: function (responseJson) {
                 // code to run if the request succeeds; parameter = response
+                var result_list = responseJson.results_tab;
+                localcounter = responseJson.newindex;
+                var cmucount = responseJson.cmucount;
+                // calc progress
+                var progress_pct = localcounter/cmucount * 100;
                 var trHTML = '';
-                $.each(responseJson, function (i, itemdata) {
-                    trHTML += '<tr><td>' + itemdata.firstName + '</td><td>' + itemdata.lastName + '</td></tr>';
+                $.each(result_list, function (i, rowdata) {
+                    var dr = rowdata.map(function(d) {return '<td>' + d + '</td>';});
+                    trHTML += '<tr>' + dr.join('') + '</tr>\n';
                 });
-                localcounter++;
                 $('#results_table').append(trHTML);
+                $('#statusdiv').append('<br/>\n' + responseJson.scanstatus
+                    + '<br/>\n' + 'INDEX: ' + localcounter + '/' + cmucount + ' = ' + progress_pct.toFixed(2) + '%'
+                    + '<br/>\n');
             },
             error: function (xhr, status) {
                 // code run if request fails; raw request and status
@@ -26,6 +34,7 @@
             }
         })
     }
+    
     (function($) { $(function() {
         $('#button1').click(getAndRenderData);
     });
