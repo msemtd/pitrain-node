bf51f99bb978a1d3bd72a106cfa43740d4808c79
Michael Erskine <michael.erskine@ketech.com>
2016-08-30 13:47:04 +0100

    Favicon and fix
    Signed-off-by: Michael Erskine <michael.erskine@ketech.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pi Major Mnemonics Learning Utility' });
 });
 
 module.exports = router;
