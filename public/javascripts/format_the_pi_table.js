a5572d0b8aa39b7dc306b35693ae421f49c45cc0
Michael Erskine <michael.erskine@ketech.com>
2016-09-07 15:27:58 +0100

    Some datatables tweaking

--- public/javascripts/format_the_pi_table.js
+++ public/javascripts/format_the_pi_table.js
@@ -7,18 +7,23 @@ $(document).ready(function() {
     var formatter = function (td, cellData, rowData, row, col) {
         $(td).html("<a href='/scanfull/"+cellData+"'>"+cellData+"</a>");
     };
+    var coldefs = [ { title: "X" }, 
+        { title: "10", "createdCell": formatter,},
+        { title: "20", "createdCell": formatter,},
+        { title: "30", "createdCell": formatter,},
+        { title: "40", "createdCell": formatter,},
+        { title: "50", "createdCell": formatter,},
+    ];
+    // Adding this footer doesn't help...
+    // var ftr = '<tfoot><tr><th></th><th></th><th></th><th></th><th></th><th></th></tr></tfoot>'
+    // $('#pi_table').append(ftr);
     $('#pi_table').DataTable( {
         data: pitab,
         ordering: false,
         pageLength: 20,
-        columns: [
-            { title: "X" }, 
-            { title: "10", "createdCell": formatter,},
-            { title: "20", "createdCell": formatter,},
-            { title: "30", "createdCell": formatter,},
-            { title: "40", "createdCell": formatter,},
-            { title: "50", "createdCell": formatter,},
-        ],
+        "lengthMenu": [ [5, 10, 15, 20, -1], [5, 10, 15, 20, "All"] ],
+        "language": {"lengthMenu": "Display _MENU_ rows"},
+        "columns": coldefs,
         "initComplete": function(settings, json) {
             // console.log( 'Table initialisation end: '+new Date().getTime() );
         },
